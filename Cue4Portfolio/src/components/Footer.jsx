302cd3230d5dd7622ed7a40a8a24f06a0a6890a5
Caliph Ferguson <caliphferguson@gmail.com>
2024-09-26 00:03:07 -0400

    footer jsx

---
+++ Cue4Portfolio/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+    return (
+      <footer>
+        <p>Connect with me:</p>
+        <a href="https://github.com/Cue4">GitHub</a>
+        <a href="mailto:caliphferguson@gmail.com">EMAIL</a>
+        <a href="https://stackoverflow.com/users/your-profile">Stack Overflow</a>
+      </footer>
+    );
+  }
+  
+  export default Footer;
