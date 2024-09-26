f5bbf27493649c0b32532c6bc57d6d92ab36616b
Caliph Ferguson <caliphferguson@gmail.com>
2024-09-25 23:14:03 -0400

    about me

--- Cue4Portfolio/src/components/AboutMe.jsx
+++ Cue4Portfolio/src/components/AboutMe.jsx
@@ -1,1 +1,12 @@
+function AboutMe() {
+    return (
+      <section>
+        <h2>About Me</h2>
+        <img src="/path-to-your-photo.jpg" alt="Developer" />
+        <p>A brief bio about yourself...</p>
+      </section>
+    );
+  }
+  
+  export default AboutMe;
   
