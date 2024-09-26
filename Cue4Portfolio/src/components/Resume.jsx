c7693dfe7ed7fb22da647ec49548892609d74203
Caliph Ferguson <caliphferguson@gmail.com>
2024-09-25 23:46:09 -0400

    resume jsx

---
+++ Cue4Portfolio/src/components/Resume.jsx
@@ -0,0 +1,17 @@
+function Resume() {
+    return (
+      <section>
+        <h2>Resume</h2>
+        <a href="/path-to-resume.pdf" download>Download Resume</a>
+        <h3>Skills</h3>
+        <ul>
+          <li>JavaScript</li>
+          <li>React</li>
+          <li>HTML & CSS</li>
+        </ul>
+      </section>
+    );
+  }
+  
+  export default Resume;
+  
